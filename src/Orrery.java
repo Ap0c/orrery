7e8f6d8bd5cb9ce7a3674b97848284ce8258afdd
Ap0c <Ap0c@users.noreply.github.com>
2015-03-25 01:18:03 +0000

    Added Planet object, created orbiting Earth.:

--- src/Orrery.java
+++ src/Orrery.java
@@ -5,25 +5,54 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Path;
 import javafx.scene.paint.Color;
+import java.util.ArrayList;
 
 // ----- Class ----- //
 
 public class Orrery extends Application {
 
+	private final double centreX = 200;
+	private final double centreY = 200;
+	private Group root;
+	private ArrayList<Planet> planets;
+
+	private void buildSun () {
+
+		Circle sun = new Circle(centreX, centreY, 40.0, Color.ORANGE);
+		root.getChildren().add(sun);
+
+	}
+
+	private void buildPlanets () {
+
+		Planet earth = new Planet("Earth", centreX, centreY, 100, 20, 5);
+		root.getChildren().add(earth.getOrbitalPath());
+		root.getChildren().add(earth.getPlanet());
+		planets.add(earth);
+
+	}
+
+	private void startMotion () {
+		for (Planet planet : planets) {
+			planet.getMotion().play();
+		}
+	}
+
 	@Override
 	public void start (Stage primaryStage) {
 
-		// Creates a circle in the centre of the screen.
-		Circle circle = new Circle(200, 200, 40, Color.ORANGE);
+		root = new Group();
+		planets = new ArrayList<Planet>();
 
-		// Sets up a group to hold the circle.
-		Group root = new Group();
-		root.getChildren().add(circle);
+		buildSun();
+		buildPlanets();
 
 		// Adds title and scene to stage.
 		primaryStage.setTitle("Solar System");
 		primaryStage.setScene(new Scene(root, 400, 400, Color.BLACK));
+		startMotion();
 		primaryStage.show();
 
 	}
---
+++ src/Planet.java
@@ -0,0 +1,82 @@
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.ArcTo;
+import javafx.scene.shape.ClosePath;
+import javafx.scene.paint.Color;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.animation.Interpolator;
+import javafx.util.Duration;
+
+class Planet {
+
+	private String name;
+	private Circle planet;
+	private Path orbitalPath;
+	private PathTransition motion;
+
+	private void setRadius (double radius) {
+		planet.setRadius(radius);
+	}
+
+	private void setColour (Color colour) {
+		planet.setFill(colour);
+	}
+
+	public void setOrbitalPath (double centreX, double centreY, double radius) {
+
+		double startY = centreY - radius;
+		double midY = centreY + radius;
+
+		orbitalPath.setStroke(Color.WHITE);
+		orbitalPath.setSmooth(true);
+
+		orbitalPath.getElements().add(new MoveTo(centreX, startY));
+		orbitalPath.getElements().add(new ArcTo(
+			radius, radius, 0, centreX, midY, false, false
+		));
+		orbitalPath.getElements().add(new ArcTo(
+			radius, radius, 0, centreX, startY, false, false
+		));
+		orbitalPath.getElements().add(new ClosePath());
+
+	}
+
+	public void setMotion (int orbitalPeriod) {
+
+		motion.setDuration(Duration.millis(orbitalPeriod * 1000));
+		motion.setPath(orbitalPath);
+		motion.setNode(planet);
+		motion.setOrientation(
+			PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+		motion.setCycleCount(Timeline.INDEFINITE);
+		motion.setInterpolator(Interpolator.LINEAR);
+
+	}
+
+	public Circle getPlanet () {
+		return planet;
+	}
+
+	public Path getOrbitalPath () {
+		return orbitalPath;
+	}
+
+	public PathTransition getMotion () {
+		return motion;
+	}
+
+	Planet (String name, double centreX, double centreY, double orbitalRadius,
+		double radius, int period) {
+
+		this.name = name;
+		this.planet = new Circle(radius, Color.LIGHTBLUE);
+		this.orbitalPath = new Path();
+		this.motion = new PathTransition();
+		setOrbitalPath(centreX, centreY, orbitalRadius);
+		setMotion(period);
+
+	}
+
+}
