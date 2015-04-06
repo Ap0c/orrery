c2f6b18c6525d7779e7d392b418b68b624d47085
Ap0c <Ap0c@users.noreply.github.com>
2015-04-06 19:49:53 +0100

    Added rest of planets.

--- src/Orrery.java
+++ src/Orrery.java
@@ -15,26 +15,44 @@ import java.util.HashMap;
 
 public class Orrery extends Application {
 
-	private final double centreX = 200;
-	private final double centreY = 200;
-	private Group root;
+	private final double centreX = 350;
+	private final double centreY = 350;
+	private Group planetGroup;
 	private Map<String, Planet> planets;
 
 	private void buildSun () {
 
-		Circle sun = new Circle(centreX, centreY, 40.0, Color.ORANGE);
-		root.getChildren().add(sun);
+		Circle sun = new Circle(centreX, centreY, 35.0, Color.ORANGE);
+		planetGroup.getChildren().add(sun);
 
 	}
 
 	private void addPlanets() {
 
+		Planet mercury = new Planet(
+			"Mercury", centreX, centreY, 50, 5, 1, new int[]{180, 180, 180});
+		planets.put("mercury", mercury);
+		Planet venus = new Planet(
+			"Venus", centreX, centreY, 90, 10, 3, new int[]{240, 200, 180});
+		planets.put("venus", venus);
 		Planet earth = new Planet(
-			"Earth", centreX, centreY, 100, 10, 5, new int[]{40, 40, 255});
+			"Earth", centreX, centreY, 130, 10, 5, new int[]{40, 100, 255});
 		planets.put("earth", earth);
 		Planet mars = new Planet(
-			"Mars", centreX, centreY, 150, 8, 7, new int[]{250, 100, 40});
+			"Mars", centreX, centreY, 170, 8, 10, new int[]{250, 100, 40});
 		planets.put("mars", mars);
+		Planet jupiter = new Planet(
+			"Jupiter", centreX, centreY, 210, 20, 60, new int[]{230, 120, 40});
+		planets.put("jupiter", jupiter);
+		Planet saturn = new Planet(
+			"Saturn", centreX, centreY, 250, 18, 150, new int[]{220, 160, 40});
+		planets.put("saturn", saturn);
+		Planet uranus = new Planet(
+			"Uranus", centreX, centreY, 290, 13, 420, new int[]{180, 200, 240});
+		planets.put("uranus", uranus);
+		Planet neptune = new Planet(
+			"Neptune", centreX, centreY, 330, 13, 825, new int[]{0, 50, 240});
+		planets.put("neptune", neptune);
 
 	}
 
@@ -43,8 +61,8 @@ public class Orrery extends Application {
 		addPlanets();
 
 		for (Map.Entry<String, Planet> planet : planets.entrySet()) {
-			root.getChildren().add(planet.getValue().getOrbitalPath());
-			root.getChildren().add(planet.getValue().getPlanet());
+			planetGroup.getChildren().add(planet.getValue().getOrbitalPath());
+			planetGroup.getChildren().add(planet.getValue().getPlanet());
 		}
 
 	}
@@ -60,7 +78,7 @@ public class Orrery extends Application {
 	@Override
 	public void start (Stage primaryStage) {
 
-		root = new Group();
+		planetGroup = new Group();
 		planets = new HashMap<>();
 
 		buildSun();
@@ -68,7 +86,7 @@ public class Orrery extends Application {
 
 		// Adds title and scene to stage.
 		primaryStage.setTitle("Solar System");
-		primaryStage.setScene(new Scene(root, 400, 400, Color.BLACK));
+		primaryStage.setScene(new Scene(planetGroup, 700, 700, Color.BLACK));
 		startMotion();
 		primaryStage.show();
 
